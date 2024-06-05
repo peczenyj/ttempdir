646235f7bc87482c1a281fed24a6bdb27d1b0878
Tiago Peczenyj <tpeczenyj@weborama.com>
2024-06-05 06:55:03 +0200

    refactor

--- analyzer/analyzer_test.go
+++ analyzer/analyzer_test.go
@@ -1,28 +1,17 @@
-package ttempdir_test
+package analyzer_test
 
 import (
-	"flag"
 	"testing"
 
 	"github.com/gostaticanalysis/testutil"
+	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/analysistest"
 
-	"github.com/peczenyj/ttempdir"
+	"github.com/peczenyj/ttempdir/analyzer"
 )
 
 // TestAnalyzer is a test for Analyzer.
 func TestAnalyzer(t *testing.T) {
-	defaults := func() map[string]string {
-		return map[string]string{
-			ttempdir.A:   "false",
-			ttempdir.MRL: "5",
-		}
-	}
-
-	t.Cleanup(func() {
-		setKV(t, &ttempdir.Analyzer.Flags, defaults())
-	})
-
 	testcases := []struct {
 		label    string
 		flags    map[string]string
@@ -30,22 +19,19 @@ func TestAnalyzer(t *testing.T) {
 	}{
 		{
 			label:    "default flags",
-			flags:    defaults(),
 			patterns: []string{"a", "b", "c"},
 		},
 		{
 			label: "flag all=true",
 			flags: map[string]string{
-				ttempdir.A:   "true",
-				ttempdir.MRL: "5",
+				analyzer.A: "true",
 			},
 			patterns: []string{"d"},
 		},
 		{
 			label: "flag max-recursion-level=10",
 			flags: map[string]string{
-				ttempdir.A:   "false",
-				ttempdir.MRL: "10",
+				analyzer.MRL: "10",
 			},
 			patterns: []string{"e"},
 		},
@@ -55,20 +41,21 @@ func TestAnalyzer(t *testing.T) {
 		tc := tc
 		t.Run(tc.label, func(t *testing.T) {
 			testdata := testutil.WithModules(t, analysistest.TestData(), nil)
-			analyzer := ttempdir.Analyzer
 
-			setKV(t, &analyzer.Flags, tc.flags)
+			ttempdirAnalyze := analyzer.New()
 
-			analysistest.Run(t, testdata, analyzer, tc.patterns...)
+			setKV(t, ttempdirAnalyze, tc.flags)
+
+			analysistest.Run(t, testdata, ttempdirAnalyze, tc.patterns...)
 		})
 	}
 }
 
-func setKV(t *testing.T, flagset *flag.FlagSet, flags map[string]string) {
+func setKV(t *testing.T, analyzer *analysis.Analyzer, flags map[string]string) {
 	t.Helper()
 
 	for k, v := range flags {
-		err := flagset.Set(k, v)
+		err := analyzer.Flags.Set(k, v)
 		if err != nil {
 			t.Fatalf("unable to set k %q v %q: %v", k, v, err)
 		}
